676cc791d337854b1a4057b139debd9a4d4cd83c
kijimaD <norimaking777@gmail.com>
2023-01-01 18:11:15 +0900

    test: add tests

---
+++ test/cl-test.el
@@ -0,0 +1,12 @@
+(require 'cl)
+
+(ert-deftest defstruct-test ()
+  ;; 構造体定義
+  (defstruct box width height)
+
+  ;; 初期化
+  (setq  b1 (make-box :width 600 :height 300))
+
+  ;; スロットにアクセスできる
+  (should (eq 600 (box-width b1)))
+  (should (eq 300 (box-height b1))))
