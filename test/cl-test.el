ea7d6d6fb78ece18ca7cd5b0790e3a781993ef2c
kijimaD <norimaking777@gmail.com>
2023-01-01 21:06:34 +0900

    fix: compile error

--- test/cl-test.el
+++ test/cl-test.el
@@ -2,11 +2,12 @@
 
 (ert-deftest defstruct-test ()
   ;; 構造体定義
-  (cl-defstruct box width height)
+  (let (b1)
+   (cl-defstruct box width height)
 
   ;; 初期化
-  (setq  b1 (make-box :width 600 :height 300))
+  (setq b1 (make-box :width 600 :height 300))
 
   ;; スロットにアクセスできる
   (should (eq 600 (box-width b1)))
-  (should (eq 300 (box-height b1))))
+  (should (eq 300 (box-height b1)))))
