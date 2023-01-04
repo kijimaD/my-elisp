76f6ba82727ac92e1bbccc2e6dc0e9d499facbf7
kijimaD <norimaking777@gmail.com>
2023-01-04 17:05:22 +0900

    fix: duplicate definition

--- test/string-test.el
+++ test/string-test.el
@@ -25,16 +25,31 @@
   (should (eq 15 (apply '+ '(1 2 3 4 5))))
   (should (eq 15 (apply '+ 1  2 '(3 4 5)))))
 
+(ert-deftest dolist-test0()
+  "
+(dolist (VAR LIST [RESULT]) BODY...)
+1: 1ループの変数
+2: リスト
+3: Body
+
+引数の形が特殊なことに注目。VARとLISTは1つのリストで、bodyとは別のリストである。こういうのはdefmacroで定義されている。
+"
+  (let (result)
+    (dolist (element '(1 2 3))
+      (setq result (cons element result)))
+    (should (equal '(3 2 1) result))
+    ))
+
 (ert-deftest dolist-test1()
+  (let (result)
+    (dolist (element '(1 2 3))
+      (setq result (cons (* element 2) result)))
+    (should (equal '(6 4 2) result))))
+
+
+(ert-deftest dolist-test2()
   "ループ処理"
   (let ((result 0))
     (dolist (num '(1 2 3 4 5))
       (setq result (+ result num)))
     (should (equal 15 result))))
-
-(ert-deftest dolist-test2()
-  "ループ処理"
-  (let (result)
-    (dolist (num '(1 2 3 4 5))
-      (setq result (cons num result)))
-    (should (equal '(5 4 3 2 1) result))))
