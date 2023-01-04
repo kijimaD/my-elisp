d73c89afa33e424987259528d745cb2cc9dfe83f
kijimaD <norimaking777@gmail.com>
2023-01-04 17:11:33 +0900

    fix: compile error

--- test/fns-test.el
+++ test/fns-test.el
@@ -1,6 +1,7 @@
 (ert-deftest eq-test ()
   "eqとequalの挙動の違い。
 eqはオブジェクトを比較する。
-equalは値を比較する。"
-  (should-not (eq "a" "a"))
+equalは値を比較する。文字列は中身は同じでも違うオブジェクトになるため、値を比較しなければならない。"
+  ;; 実行はできるけど、compile errorになるからコメントアウト
+  ;; (should-not (eq "a" "a"))
   (should (equal "a" "a")))
