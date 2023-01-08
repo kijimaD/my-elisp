9d697a8bfc743594914436f605019b5f76dc9c8a
kijimaD <norimaking777@gmail.com>
2023-01-09 01:11:29 +0900

    refactor: put away temporary file

--- test/f-test.el
+++ test/f-test.el
@@ -10,15 +10,18 @@
 (ert-deftest f-read-test ()
   "ファイルの中身を書き込む+読み込む"
   (f-write-text "content" 'utf-8 "./tmp")
-  (should-not (zerop (length (f-read-text "tmp")))))
+  (should-not (zerop (length (f-read-text "tmp"))))
+  (if (f-file? "tmp") (delete-file "tmp")))
 
 (ert-deftest f-file?-test ()
   (f-write-text "content" 'utf-8 "./tmp")
   (should (eq t (f-file? "tmp")))
-  (should (eq nil (f-file? "not exist"))))
+  (should (eq nil (f-file? "not exist")))
+  (if (f-file? "tmp") (delete-file "tmp")))
 
 (ert-deftest f-ext?-test ()
   (let ((tmp "tmp.el"))
     (with-temp-file tmp)
     (should (eq t (f-ext? tmp "el")))
-    (should (eq nil (f-ext? tmp "org")))))
+    (should (eq nil (f-ext? tmp "org")))
+    (if (f-file? tmp) (delete-file tmp))))
