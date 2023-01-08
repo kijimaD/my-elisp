8be68a54a97509991e7ec70b5e9d07da2c5f5e4f
kijimaD <norimaking777@gmail.com>
2023-01-08 13:59:09 +0900

    test: add test

--- test/with-macro-test.el
+++ test/with-macro-test.el
@@ -4,7 +4,7 @@
          (lambda (alias)
            `(,alias                     ; 関数名
              (string)                   ; 引数
-             ,(list 'backquote (list 'message '(concat "<b>" ',string "/<b>"))))) ; 本体
+             ,(list 'backquote (list 'concat "<" (symbol-name alias) ">" ',string "</" (symbol-name alias) ">")))) ; 本体
          '(black)
          )
      ,(cons 'my-ansi--concat body)))
@@ -12,10 +12,7 @@
 (defun my-ansi--concat (&rest sequences)
   (apply #'concat (cl-remove-if-not 'stringp sequences)))
 
-(my-with-ansi "this is " (black "black") " text")
-
-;; (ert-deftest my-with-ansi-test ()
-;;   (let ((result (my-with-ansi "this is " (black "black") " text")))
-;;     (should (string= "this is <b>black</b> text" result))
-;;     )
-;;   )
+(ert-deftest my-with-ansi-test ()
+  (let ((result (my-with-ansi "this is " (black "text"))))
+    (should (string= "this is <black>text</black>" result))
+    ))
