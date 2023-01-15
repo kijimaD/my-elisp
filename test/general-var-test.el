e6256922014d88f9c1200f5a5de45eee922d9bb6
kijimaD <norimaking777@gmail.com>
2023-01-15 20:15:13 +0900

    fix: `‘eq’ is an obsolete generalized variable.`

--- test/general-var-test.el
+++ test/general-var-test.el
@@ -1,8 +1,5 @@
 (ert-deftest general-var-test ()
   "setfは汎変数マクロ"
-  (let (i)
-    (setf (eq i 4) t)
-    (should (eq 4 i)))
   (let ((list '("1" "2" "3")))
     (setf (elt list 1) "10")
     (should (equal '("1" "10" "3") list))
